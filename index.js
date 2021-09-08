1d52ae9adf40fcba42169b290b8789ce9e665065
bachtangthang <thang5217113@gmail.com>
2021-09-08 21:41:01 +0700

    add control mouse, random object generator

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { 
+    Engine, 
+    Render, Runner,
+    World, 
+    Bodies, 
+    MouseConstraint, 
+    Mouse 
+} = Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const {world} = engine;
 const render = Render.create({
     element: document.body,
     engine: engine,
-    opion: {
-        width: 800,
-        height: 600,
+    options: {
+        wireframes: false,
+        width,
+        height,
     }
 });
 
 Render.run(render);
 Runner.run(Runner.create(),engine);
+
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+}))
 
 //WALLS
 const walls = [
@@ -32,4 +47,11 @@ const walls = [
 
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200,200,50,50));
+//Random shapes
+
+for(let i =0; i<=20;i++){
+    World.add(world, Bodies.rectangle(Math.random() * width,Math.random()*height ,50,50));
+    
+}
+
+
