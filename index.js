260673b968cea168b44742cfe34c7eaf40208215
bachtangthang <thang5217113@gmail.com>
2021-09-08 22:19:03 +0700

    delete mouse control and generating random object

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const { 
     Engine, 
-    Render, Runner,
+    Render, 
+    Runner,
     World, 
     Bodies, 
-    MouseConstraint, 
-    Mouse 
 } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -24,11 +23,6 @@ const render = Render.create({
 
 Render.run(render);
 Runner.run(Runner.create(),engine);
-
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}))
-
 //WALLS
 const walls = [
     Bodies.rectangle(400,0,800,40, {
@@ -47,11 +41,7 @@ const walls = [
 
 World.add(world, walls);
 
-//Random shapes
 
-for(let i =0; i<=20;i++){
-    World.add(world, Bodies.rectangle(Math.random() * width,Math.random()*height ,50,50));
-    
-}
 
+  
 
